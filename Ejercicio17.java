5beae5df1488aeb117488bc9551910481e12a1d2
santisilba4 <138083614+santisilba4@users.noreply.github.com>
2023-06-29 21:42:31 -0300

    Update and rename 17.java to Ejercicio17.java

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PromedioEvaluaciones {
+public class Ejercicio17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
