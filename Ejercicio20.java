cf50a2ff6826baf7d5030ebc3345a748ed226b8c
santisilba4 <138083614+santisilba4@users.noreply.github.com>
2023-06-29 21:42:57 -0300

    Update and rename 20.java to Ejercicio20.java

--- Ejercicio20.java
+++ Ejercicio20.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class NumerosAlAzar {
+public class Ejercicio20 {
     public static void main(String[] args) {
         Random random = new Random();
         int contador = 0;
