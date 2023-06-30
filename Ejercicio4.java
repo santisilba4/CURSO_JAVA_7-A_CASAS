0852e5a615397f0d6a27454944f6ae56bc02d7ec
santisilba4 <138083614+santisilba4@users.noreply.github.com>
2023-06-29 21:39:26 -0300

    Update and rename 3.java to Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CantidadDiasMes {
+public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
