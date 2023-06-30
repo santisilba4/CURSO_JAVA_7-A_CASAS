148ccf461e5ff0d0f921a4940b410ef431efaafd
santisilba4 <138083614+santisilba4@users.noreply.github.com>
2023-06-29 21:40:53 -0300

    Update and rename 8.java to Ejercicio8.java

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JuegoPiedraPapelTijera {
+public class Ejercicio8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
